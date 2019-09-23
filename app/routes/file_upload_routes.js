dd28a3a96fa491162d6c6e7230436024cacfa1bb
michelleRahman21 <michgomez1807@hotmail.com>
2019-09-23 16:29:02 -0400

    Create file upload models and routes
    Create fileUpload mongoose model to store name, data, type and url of
    file
    
    Copy example routes file and rename 'examples' to 'fileUploads' etc.
    Use multer to accept files into POST route
    fileUploadApi file to upload Buffer to s3
    Tell server.js to use the fileUpload routes

---
+++ app/routes/file_upload_routes.js
@@ -0,0 +1,112 @@
+
+const express = require('express')
+
+const passport = require('passport')
+const fileUploadApi = require('../../lib/fileUploadApi')
+const multer = require('multer')
+const upload = multer({ storage: multer.memoryStorage() })
+
+const FileUpload = require('../models/file_upload')
+
+const customErrors = require('../../lib/custom_errors')
+
+// we'll use this function to send 404 when non-existant document is requested
+const handle404 = customErrors.handle404
+// we'll use this function to send 401 when a user tries to modify a resource
+// that's owned by someone else
+const requireOwnership = customErrors.requireOwnership
+
+// this is middleware that will remove blank fields from `req.body`, e.g.
+// { example: { title: '', text: 'foo' } } -> { example: { text: 'foo' } }
+const removeBlanks = require('../../lib/remove_blank_fields')
+// passing this as a second argument to `router.<verb>` will make it
+// so that a token MUST be passed for that route to be available
+// it will also set `req.user`
+const requireToken = passport.authenticate('bearer', { session: false })
+
+// instantiate a router (mini app that only handles routes)
+const router = express.Router()
+
+// INDEX
+// GET /examples
+router.get('/fileUploads', requireToken, (req, res, next) => {
+  FileUpload.find()
+    .then(fileUploads => {
+      // `examples` will be an array of Mongoose documents
+      // we want to convert each one to a POJO, so we use `.map` to
+      // apply `.toObject` to each one
+      return fileUploads.map(fileUpload => fileUploads.toObject())
+    })
+    // respond with status 200 and JSON of the examples
+    .then(fileUploads => res.status(200).json({ fileUploads: fileUploads }))
+    // if an error occurs, pass it to the handler
+    .catch(next)
+})
+
+// SHOW
+// GET /examples/5a7db6c74d55bc51bdf39793
+router.get('/fileUploads/:id', requireToken, (req, res, next) => {
+  // req.params.id will be set based on the `:id` in the route
+  FileUpload.findById(req.params.id)
+    .then(handle404)
+    // if `findById` is succesful, respond with 200 and "example" JSON
+    .then(fileUpload => res.status(200).json({ fileUpload: fileUpload.toObject() }))
+    // if an error occurs, pass it to the handler
+    .catch(next)
+})
+
+// CREATE
+// POST /examples
+router.post('/fileUploads', upload.single('upload'), (req, res, next) => {
+  // req.body.fileUpload.owner = req.user.id
+  console.log(req.file)
+  fileUploadApi(req.file.originalname, req.file.buffer)
+    .then(console.log)
+  // FileUpload.create(req.body.fileUpload)
+  // .then(fileUpload => {
+  //     res.status(201).json({ fileUpload: fileUpload.toObject() })
+  //   })
+    .catch(next)
+})
+
+// UPDATE
+// PATCH /examples/5a7db6c74d55bc51bdf39793
+router.patch('/fileUploads/:id', requireToken, removeBlanks, (req, res, next) => {
+  // if the client attempts to change the `owner` property by including a new
+  // owner, prevent that by deleting that key/value pair
+  delete req.body.example.owner
+
+  FileUpload.findById(req.params.id)
+    .then(handle404)
+    .then(fileUpload => {
+      // pass the `req` object and the Mongoose record to `requireOwnership`
+      // it will throw an error if the current user isn't the owner
+      requireOwnership(req, fileUpload)
+
+      // pass the result of Mongoose's `.update` to the next `.then`
+      return fileUpload.updateOne(req.body.fileUpload)
+    })
+    // if that succeeded, return 204 and no JSON
+    .then(() => res.sendStatus(204))
+    // if an error occurs, pass it to the handler
+    .catch(next)
+})
+
+// DESTROY
+// DELETE /examples/5a7db6c74d55bc51bdf39793
+router.delete('/fileUploads/:id', requireToken, (req, res, next) => {
+  FileUpload.findById(req.params.id)
+    .then(handle404)
+    .then(fileUpload => {
+      // throw an error if current user doesn't own `example`
+      requireOwnership(req, fileUpload)
+      // delete the example ONLY IF the above didn't throw
+      fileUpload.deleteOne()
+    })
+    // send back 204 and no content if the deletion succeeded
+    .then(() => res.sendStatus(204))
+    // if an error occurs, pass it tfileUploadandler
+    .catch(next)
+})
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,12 +1,17 @@
+
 // require necessary NPM packages
 const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const cors = require('cors')
+const AWS = require('aws-sdk')
+
+AWS.config.update({region: 'us-east-1'})
 
 // require route files
 const exampleRoutes = require('./app/routes/example_routes')
 const userRoutes = require('./app/routes/user_routes')
+const fileUploadRoutes = require('./app/routes/file_upload_routes')
 
 // require middleware
 const errorHandler = require('./lib/error_handler')
@@ -65,6 +70,7 @@ app.use(requestLogger)
 // register route files
 app.use(exampleRoutes)
 app.use(userRoutes)
+app.use(fileUploadRoutes)
 
 // register error handling middleware
 // note that this comes after the route middlewares, because it needs to be
